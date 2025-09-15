b92925968ab219318e34a69731260b30d60eae76
Gabriela <gabriela.carniel@sellflux.com.br>
2025-09-15 18:11:21 -0300

    chore: update dependencies and improve environment variable handling

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,16 +1,10 @@
 import z from "zod";
 
 const envSchema = z.object({
-	NODE_ENV: z.enum(["development", "production"]),
-	PORT: z.number().default(3333),
+	PORT: z.coerce.number().default(3333),
+	OPENAI_API_KEY: z.string(),
+	DEEPSEEK_API_KEY: z.string(),
+	OPENROUTER_API_KEY: z.string(),
 });
 
-const _env = envSchema.safeParse(process.env);
-
-if (_env.success === false) {
-	console.error("Invalid environment variables", _env.error);
-
-	throw new Error("Invalid environment variables");
-}
-
-export const env = _env.data;
+export const env = envSchema.parse(process.env);
--- src/routes/generate-note.ts
+++ src/routes/generate-note.ts
@@ -1,7 +1,17 @@
+import { deepseek } from "@ai-sdk/deepseek";
+import { openrouter } from "@openrouter/ai-sdk-provider";
+import { generateText } from "ai";
 import { FastifyInstance } from "fastify";
 
 export async function generateNote(app: FastifyInstance) {
-	app.get("/", (request, reply) => {
-		reply.send({ text: "hello world" });
+	app.post("/ai", async (request, reply) => {
+		const { prompt } = request.body as { prompt: string };
+		const result = await generateText({
+			model: openrouter.chat("deepseek/deepseek-chat-v3.1:free"),
+			prompt,
+			system: "Voce é uma ia especializada em resumir coisas de forma sucinta",
+		});
+
+		reply.send({ message: result });
 	});
 }
